84d0c6e7b30b0682061ef5a261de42a84f47d5cf
samueldamatta <samueldamattaid@gmail.com>
2025-11-04 22:15:03 -0300

    Implement SobreNosController, update routes, and add TesteController with view

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\TesteController;
 
-Route::get('/', 'PrincipalController')->name('site.index');
-Route::get('/sobrenos', 'SobreNosController')->name('site.sobrenos');
-Route::get('/contato', 'ContatoController')->name('site.contato');
+
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobrenos', [SobreNosController::class, 'sobrenos'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::get('/login', function () {return 'Login';})->name('site.login');
 
 // App
@@ -14,6 +19,11 @@ Route::prefix('/app')->group(function() {
     Route::get('/produtos', function () {return 'Produtos';})->name('app.produtos');
 });
 
-Route::get('rota1', function(){}->name('site.rota1'));
-Route::get('rota2', function(){}->name('site.rota2'));
+Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('site.teste');
+
+Route::fallback(function (){
+    echo 'Não existe';
+});
+
+
 
